015c6bb2790c87610e0f1f57125a709e55f224e9
tzirulnicov <tzirulnicov@mail.ru>
2025-02-16 14:59:07 +0300

    manage

--- django_web_service/manage.py
+++ django_web_service/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoTzitRu.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Esp12fScd30.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
